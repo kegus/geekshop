1fd93849072e3f9c73820721bd8bc3cfec4d657d
kegus <kegus@mail.ru>
2021-02-10 13:48:42 +0600

    Lesson 7 second part

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from mainapp.models import ProductCategory, Product
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+# admin.site.register(ProductCategory)
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    readonly_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'quantity',)
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category',)
+    readonly_fields = ('short_description',)
+    ordering = ('price', '-name',)
+    search_fields = ('name', 'category__name',)
